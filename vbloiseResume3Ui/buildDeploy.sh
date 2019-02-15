03a744ebfd73260e1cd4257b539942386c0ae4b1
Vince Bloise <vbloise3@gmail.com>
2019-02-15 08:22:14 -0500

    Cleaned up build deploy script

--- vbloiseResume3Ui/buildDeploy.sh
+++ vbloiseResume3Ui/buildDeploy.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 #/bin/bash
 #build the chalice-javascript-jdk
-cd ~/vbloiseResume3/vbloiseResume3Ui/src/chalice-javascript-sdk/
-rm -r *
-cd ~/vbloiseResume3/vbloiseResume3Ui/src/
-rmdir chalice-javascript-sdk
-cd ~/vbloiseResume3/
-chalice generate-sdk ./vbloiseResume3Ui/src
+#cd ~/vbloiseResume3/vbloiseResume3Ui/src/chalice-javascript-sdk/
+#rm -r *
+#cd ~/vbloiseResume3/vbloiseResume3Ui/src/
+#rmdir chalice-javascript-sdk
+#cd ~/vbloiseResume3/
+#chalice generate-sdk ./vbloiseResume3Ui/src
 #build the site
-cd ~/vbloiseResume3/vbloiseResume3Ui/
+#cd ~/vbloiseResume3/vbloiseResume3Ui/
 #build the site
 ng build --prod --aot
 #upload files
